dae6d69673a13b4ca0393ce1b58b4e594370393c
ZoxxxDev <mathias.bar1809@gmail.com>
2023-08-08 14:47:37 +0200

    fix syntax errors sql

--- server/database.sql
+++ server/database.sql
@@ -1,49 +1,49 @@
 -- Create the table here then copy/past on terminal
 
 
-CREATE TABLE [IF NOT EXISTS] users (
+CREATE TABLE users (
     userId serial PRIMARY KEY,
     userMail VARCHAR (150) NOT NULL,
-    userPassword VARCHAR (150) NOT NULL,
+    userPassword VARCHAR (150) NOT NULL
 );
 
-CREATE TABLE [IF NOT EXISTS] Cars (
+CREATE TABLE Cars (
    carId serial PRIMARY KEY,
    carBrand VARCHAR(25) NOT NULL,
    carModel VARCHAR(25) NOT NULL,
-   circulationDate int(4) NOT NULL,
+   circulationDate int NOT NULL,
    engine VARCHAR (15),
-   distanceTravel int (8) NOT NULL
+   distanceTravel int NOT NULL
 );
 
-CREATE TABLE [IF NOT EXISTS] schedule(
+CREATE TABLE schedule(
     dayName VARCHAR (10),
-    hourStart DATE TIME,
-    hourPause DATE TIME,
-    hourStopPAUSE DATE TIME,
-    hourStop DATE TIME,
+    hourStart TIME,
+    hourPause TIME,
+    hourStopPAUSE TIME,
+    hourStop TIME
 );
 
 
-CREATE TABLE [IF NOT EXISTS]  presInfo(
+CREATE TABLE presInfo(
     infoId serial PRIMARY KEY,
     infoTitle VARCHAR(50) NOT NULL,
     infoText VARCHAR (350) NOT NULL
 );
 
-CREATE TABLE [IF NOT EXISTS]  noticeMessage(
+CREATE TABLE noticeMessage(
     noticeId serial PRIMARY KEY,
     noticeUserNAME VARCHAR(50) NOT NULL,
     noticeUserLASTNAME VARCHAR (50) NOT NULL,
     noticeUserMessage VARCHAR (350) NOT NULL
 );
 
-CREATE TABLE [IF NOT EXISTS]  carsMessage(
+CREATE TABLE carsMessage(
     carMessageId serial PRIMARY KEY,
     carUserNAME VARCHAR(50) NOT NULL,
     carUserLASTNAME VARCHAR (50) NOT NULL,
     carUserMAIL VARCHAR (50) NOT NULL,
     carUserMessage VARCHAR (350) NOT NULL,
     dateMeet DATE,
-    hourMeet DATE TIME,
+    hourMeet DATE
 );
