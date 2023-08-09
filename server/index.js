abe525509d97391679451f4092c0b331eeadc9cb
ZoxxxDev <mathias.bar1809@gmail.com>
2023-08-09 20:33:52 +0200

    Add CRUD : cars, info

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use(express.json()); // req.body
 
 
 // Tout les appel API
+// Users routes for admin
 // Create user
 app.post("/users", async(req, res) => {
     try {
@@ -123,9 +124,80 @@ app.delete("/infos/:id", async(req,res) =>{
 });
 
 // Schedule (Dayname | hourstart | hourpause | hourstoppause | hourstop)(Get & Update)
+// Get
+app.get("/schedule", async(req,res) =>{
+    try {
+        const getSchedule = await pool.query("SELECT * FROM schedule");
+        
+        res.json(getSchedule.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// Update
+app.put("/schedule/:id", async(req, res) =>{
+    try {
+        const {id} = req.params;
+        
+        const {hourstart, hourpause, hourstoppause, hourstop} = req.body;
+        console.log(req.body);
+        console.log(hourstart);
+        const dayUpdate = await pool.query("UPDATE schedule SET (hourstart, hourpause, hourstoppause, hourstop) = ($1,$2,$3,$4) WHERE dayname = $5",
+        [hourstart, hourpause, hourstoppause, hourstop, id]);
+        
+        res.json("Day time update");
+
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// Cars (post / get / delete) (carid (primarikey) | carbrand | carmodel | circulationdate | engine | distancetravel)
+// get cars
+app.get("/cars", async(req, res) =>{
+    try {
+        const getCars = await pool.query("SELECT * FROM cars");
+
+        console.log("we get cars");
+        res.json(getCars.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// post new car
+app.post("/cars", async(req, res) =>{
+    try {
+        const {carbrand, carmode, circulationdate, engine, distancetravel} = req.body;
+
+        const createCar = await pool.query("INSERT INTO cars (carbrand, carmodel, circulationdate, engine, distancetravel) VALUES($1, $2, $3, $4, $5)",
+        [carbrand, carmode, circulationdate, engine, distancetravel]);
+
+        res.json(createCar.rows[0]);
+
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// delete car
+app.delete("/cars/:id", async(req, res) =>{
+    try {
+        const {id} = req.params;
+
+        const deleteCar = await pool.query("DELETE FROM cars WHERE carid = $1", [id])
+
+        res.json("car deleted");
+
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 
 
+// Comment all
 // Listening app
 app.listen(5000, () =>{
     console.log("server start port : 5000")
