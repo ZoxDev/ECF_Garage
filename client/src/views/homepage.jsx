608542fa0a9efc17a1d35110d8d89e772c8ca223
ZoxxxDev <mathias.bar1809@gmail.com>
2023-08-13 12:35:20 +0200

    Now notice card can be instaciate (max of 15 card) Start : swiperjs

--- client/src/views/homepage.jsx
+++ client/src/views/homepage.jsx
@@ -13,30 +13,35 @@ import Noticecard from '../components/noticecard'
 
 function Homepage() {
 
-    // TO FIX ! PSEUDO ELEMENTS NOT WORKING
-    return (
-      <>
-        <Navbar />
-        {/* First section */}
-        <section className="first-section ">
-        <p className='text'>Faites de votre véhicule un endroit sur et agréable.</p>
-        <ServicesButton link="#second"/>
-        </section>
-
-        {/* Second section */}
-        <section id='second' className="second-section">
-          <Info infid={0} url={motor}/>
-          <Info infid={1} url={entretien}/>
-          <Info infid={2} url={cardeal}/>
-        </section>
-
-        {/* Third section */}
-        <section className='third-section'>
-        <Banderole/>
-        <Noticecard notid={0}/>
-        </section>
-      </>
-    )
+  // Create an array that show notice in function of there id / max of 10 notice
+  const noticeCardArray = []
+  for(let i = 0 ; i < 10; i++){
+    noticeCardArray.push(<Noticecard notid={i}/>)
   }
-  
-  export default Homepage
+
+  return (
+    <>
+      <Navbar />
+      {/* First section */}
+      <section className="first-section ">
+        <p className='text'>Faites de votre véhicule un endroit sur et agréable.</p>
+        <ServicesButton link="#second" />
+      </section>
+
+      {/* Second section */}
+      <section id='second' className="second-section">
+        <Info infid={0} url={motor} />
+        <Info infid={1} url={entretien} />
+        <Info infid={2} url={cardeal} />
+      </section>
+
+      {/* Third section */}
+      <section className='third-section'>
+        <Banderole />
+        {noticeCardArray}
+      </section>
+    </>
+  )
+}
+
+export default Homepage
