e852f0bbf32ed1429b5562234cf27e3a342a3dd3
ZoxxxDev <mathias.bar1809@gmail.com>
2023-08-14 15:25:59 +0200

    Start the footer basics & Add the scedule to it

---
+++ client/src/components/schedule.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+
+export default function Schedule(props) {
+
+    // Get info in an array 
+    const [schedule, setSchedule] = useState([]);
+
+    // Request
+    const getSchedule = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/schedule");
+            const jsonData = await response.json();
+
+            setSchedule(jsonData);
+            console.log(jsonData);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    // Request do once
+    useEffect(() => {
+        getSchedule();
+    }, [])
+
+    //Check if the value is available
+    const selectedSchedule = schedule[props.dayid];
+    const dayname = selectedSchedule ? selectedSchedule.dayname : "";
+    const hourstart = selectedSchedule ? selectedSchedule.hourstart : "";
+    const hourpause = selectedSchedule ? selectedSchedule.hourpause : "";
+    const hourstoppause = selectedSchedule ? selectedSchedule.hourstoppause : "";
+    const hourstop = selectedSchedule ? selectedSchedule.hourstop : "";
+
+
+    return (
+        <>
+            <tr>
+                <td>
+                    {dayname}
+                </td>
+                <td>
+                    {hourstart}
+                </td>
+                <td>
+                    {hourpause}
+                </td>
+                <td>
+                    {hourstoppause}
+                </td>
+                <td>
+                    {hourstop}
+                </td>
+            </tr>
+
+
+        </>
+    );
+
+}
--- client/src/views/footer.jsx
+++ client/src/views/footer.jsx
@@ -1,13 +1,42 @@
+import '../components/css/footer.css'
+import '../../src/index.css'
 
-
+import { useEffect, useState } from 'react';
+import Schedule from '../components/schedule';
 
 
 export default function Footer() {
 
 
-    return(
+
+    return (
         <>
-        
+            <footer>
+                <p className="text-footer">VOUS VOULEZ SAVOIR OU ON EST ? NOUS LAISSE UN AVIS ?<br />TOUT EST LÀ</p>
+                <button className='button-footer'>UN AVIS ?</button>
+                <div className='schedule'>
+                    <table className=" text-white">
+                        <thead>
+                            <tr>
+                                <th>Jours</th>
+                                <th>Début</th>
+                                <th>Pause-midi</th>
+                                <th>Pause-midi</th>
+                                <th>Fin</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* In function of the name of day (todolater) */}
+                            <Schedule dayid={0}/>
+                            <Schedule dayid={1}/>
+                            <Schedule dayid={2}/>
+                            <Schedule dayid={3}/>
+                            <Schedule dayid={5}/>
+                            <Schedule dayid={4}/>
+                        </tbody>
+                    </table>
+                </div>
+            </footer>
         </>
     );
 }
