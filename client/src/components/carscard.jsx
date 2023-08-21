e406b7d9dfd004c2025035503835b9a84b498174
ZoxxxDev <mathias.bar1809@gmail.com>
2023-08-21 10:28:03 +0200

    Fetch data for card component

--- client/src/components/carscard.jsx
+++ client/src/components/carscard.jsx
@@ -3,22 +3,26 @@ import '../components/css/carscard.css'
 
 // Utilites
 import { useState, useEffect } from 'react'
+import { useFetch } from './queryget';
 
-export default function Carscard(props) {
+export default function Carscard() {
+
+    const [data, loading, error] = useFetch("http://localhost:5000/cars")
+
+    useEffect(() =>{
+        console.log(data)
+    }, [])
+
 
 
     return (
         <>
             <div className='cards-container'>
                 <div className='card-image'>
-                    {/* Image of the card */}
+                    IMAGE
                 </div>
                 <div className='card-content'>
 
-                </div>
-
-                <div className='btn-card'>
-                    {/* Button for message a car */}
                 </div>
             </div>
         </>
