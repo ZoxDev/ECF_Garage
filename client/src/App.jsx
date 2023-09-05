3960d19f100dd16d187abebd95d3a964cc0f7c78
ZoxxxDev <mathias.bar1809@gmail.com>
2023-09-05 20:21:29 +0200

    Finish the bascis routes for jwtAuth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,11 @@ import Homepage from "./views/homepage"
 import CarsPage from "./views/carspage"
 import Noticepage from "./views/noticepage"
 
-// Router
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom'
-
 function App() {
 
   return (
     <>
-     <Homepage></Homepage>
+    <Homepage></Homepage>
     </>
   )
 }
