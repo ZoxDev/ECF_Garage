4a63bcac7f0e9984ac6b7d2bde5e8a89f5ad51ba
ZoxxxDev <mathias.bar1809@gmail.com>
2023-09-05 17:22:13 +0200

    Start Routing auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,13 @@ import Homepage from "./views/homepage"
 import CarsPage from "./views/carspage"
 import Noticepage from "./views/noticepage"
 
+// Router
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
 
 function App() {
 
