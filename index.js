f1ac0ee943af7e9792e3f324aab48908ec15888b
ZoxxxDev <mathias.bar1809@gmail.com>
2023-09-21 13:24:01 +0200

    fixx

--- index.js
+++ index.js
@@ -12,14 +12,17 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "client/dist")));
+app.get('*', function (req, res) {
+    res.sendFile('index.html', { root: path.join(__dirname, 'client', 'dist') });
+});
 
 // Production
 if (process.env.NODE_ENV === "production") {
     // server static content
     app.use(express.static(path.join(__dirname, "client/dist")));
-//     app.get('/*', function (req, res) {
-//         res.sendFile('index.html', { root: path.join(__dirname, 'client', 'dist') });
-//     });
+    //     app.get('/*', function (req, res) {
+    //         res.sendFile('index.html', { root: path.join(__dirname, 'client', 'dist') });
+    //     });
 }
 
 // Create user JWT AUTH
