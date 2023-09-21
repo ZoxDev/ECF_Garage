4178d621faaf96acbebafe8af8e96d03ce6b6f2d
ZoxxxDev <mathias.bar1809@gmail.com>
2023-09-21 15:40:28 +0200

    Change path for image

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.get("/cars", async (req, res) => {
 // post new car
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'client/src/server/images')
+        cb(null, './client/dist/assets')
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname + '.png')
